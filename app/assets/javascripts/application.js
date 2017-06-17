4e1d4e58ace1a9d440164725589a8ccb770d6d3a
Gustavo David <gustavodev@outlook.com.br>
2017-06-17 20:44:29 -0300

    Alteração para suportar ação de exclusão de um registro e deletar sessão com verbo 'delete'

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap_sb_admin_base_v2
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
