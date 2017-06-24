06e77af00ad2ae05dd9af7d0b7fc79f80d77c0ae
Gustavo David <gustavodev@outlook.com.br>
2017-06-24 16:59:57 -0300

    colocando o atributo filme_id no json da sala

--- app/serializers/sala_serializer.rb
+++ app/serializers/sala_serializer.rb
@@ -1,7 +1,5 @@
 class SalaSerializer < ActiveModel::Serializer
-  attributes :id, :tipo, :lingua, :filme, :cinema_id
+  attributes :id, :tipo, :lingua, :filme_id, :cinema_id
 
   has_many :horarios
-  has_one :cinema
-  has_one :filme
 end
